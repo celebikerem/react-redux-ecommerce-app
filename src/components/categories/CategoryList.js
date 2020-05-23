5995cea3084fa3f6752650c76e256a9652ac099b
Kerem Çelebi <kcelebi94@gmail.com>
2020-05-24 01:49:47 +0300

    Get categories method created and categories listed.

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
+
   render() {
     return (
       <div>
         <h3>Categories</h3>
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem>{category.categoryName}</ListGroupItem>
+          ))}
+        </ListGroup>
         <h5>Selected category: {this.props.currentCategory.categoryName}</h5>
       </div>
     );
@@ -15,7 +27,19 @@ class CategoryList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
   };
 }
 
-export default connect(mapStateToProps)(CategoryList);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -2,4 +2,5 @@ export default {
   currentCategory: {
     categoryName: "Default Category",
   },
+  categories: [],
 };
