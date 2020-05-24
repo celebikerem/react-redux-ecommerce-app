c938d47598d33b4efd9406a6c6bd1c4ad4563005
Kerem Çelebi <kcelebi94@gmail.com>
2020-05-25 02:34:44 +0300

    Add to cart event created. AlertifyJS added.

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as productActions from "../../redux/actions/productActions";
-import { Badge, Table } from "reactstrap";
+import * as cartActions from "../../redux/actions/cartActions";
+import { Badge, Table, Button } from "reactstrap";
+import alertify from "alertifyjs";
 
 class ProductList extends Component {
   componentDidMount() {
     this.props.actions.getProducts();
   }
+
+  addToCart = (product) => {
+    this.props.actions.addToCart({ quantity: 1, product });
+    alertify.success(product.productName + "added to cart.");
+  };
 
   render() {
     return (
@@ -16,29 +23,38 @@ class ProductList extends Component {
           <Badge color="success">
             {this.props.currentCategory.categoryName}
           </Badge>
-          <Table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Product Name</th>
-                <th>Unit Price</th>
-                <th>Quantity</th>
-                <th>Units in Stock</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.prodcts.map((product) => (
-                <tr key={product.id}>
-                  <th scope="row">{product.id}</th>
-                  <td>{product.productName}</td>
-                  <td>{product.unitPrice}</td>
-                  <td>{product.quantityPerUnit}</td>
-                  <td>{product.unitsInStock}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
         </h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Unit Price</th>
+              <th>Quantity</th>
+              <th>Units in Stock</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.prodcts.map((product) => (
+              <tr key={product.id}>
+                <th scope="row">{product.id}</th>
+                <td>{product.productName}</td>
+                <td>{product.unitPrice}</td>
+                <td>{product.quantityPerUnit}</td>
+                <td>{product.unitsInStock}</td>
+                <td>
+                  <Button
+                    onClick={() => this.addToCart(product)}
+                    color="success"
+                  >
+                    Add to Cart
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
@@ -55,6 +71,7 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       getProducts: bindActionCreators(productActions.getProducts, dispatch),
+      addToCart: bindActionCreators(cartActions.addToCart, dispatch),
     },
   };
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import configureStore from "./redux/reducers/configureStore";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "alertifyjs/build/css/alertify.min.css";
 
 const store = configureStore();
 ReactDOM.render(
